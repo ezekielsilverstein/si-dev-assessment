c4e19a7ac993247d2f8b69efb06fc5e1ba986651
Zeke <ezekiel.m.silverstein@gmail.com>
2017-05-05 20:26:09 +0000

    Finishing Task 6 (exluding environment)
    Submission successfully sends sms via Twilio!
    Print out SMS params on sms.html.erb rather than explicitly in pages_controllers.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,19 @@
+require 'rubygems'
+require 'twilio-ruby'
+require 'json'
+
+
 class PagesController < ApplicationController
   def home
   end
 
   def sms
+    account_sid = "AC3f3e3c18f526ecfd6b0e038c400382cf"
+    auth_token = "dfa29c58e7b3d417ce69c9d8ab0f5f20"
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    message = @client.account.messages.create(
+      :body => "#{params[:text]}",
+      :to => "+1" + "#{params[:num]}",
+      :from => "+16178588362")
   end
 end
