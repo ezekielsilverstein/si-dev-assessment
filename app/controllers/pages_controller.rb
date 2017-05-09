e085783d0ab922f5d6988f267342f62bca73342d
Zeke <ezekiel.m.silverstein@gmail.com>
2017-05-09 18:40:10 +0000

    Commented out ENV VARS

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,12 +8,12 @@ class PagesController < ApplicationController
   end
 
   def sms
-    account_sid = "AC3f3e3c18f526ecfd6b0e038c400382cf"
-    auth_token = "dfa29c58e7b3d417ce69c9d8ab0f5f20"
-    @client = Twilio::REST::Client.new account_sid, auth_token
+    #@client = Twilio::REST::Client.new String(ENV["ACCOUNT_SID"]), String(ENV["AUTH_TOKEN"])
+    @client = Twilio::REST::Client.new "AC3f3e3c18f526ecfd6b0e038c400382cf", "dfa29c58e7b3d417ce69c9d8ab0f5f20"
     message = @client.account.messages.create(
       :body => "#{params[:text]}",
       :to => "+1" + "#{params[:num]}",
+      #:from => String(ENV["FROM_NUM"]))
       :from => "+16178588362")
   end
 end
